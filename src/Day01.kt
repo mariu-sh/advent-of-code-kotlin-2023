d4eee222b0823139d01eb4d55298e59fa7b5509d
arcolinuxz <arcolinuxinfo@gmail.com>
2023-12-11 20:46:28 +0100

    prepare test for part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -10,7 +10,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+    check(part2(readInput("Day01_p2_test")) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
